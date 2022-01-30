80149eb3c7b5f711e4e08140291caaa2a570441d
Tega Amrore <otega.amrore@gmail.com>
2022-01-30 14:31:08 +0000

    misc changes

--- index.php
+++ index.php
@@ -4,5 +4,13 @@
 </head>
 <body>
   hello world
+  <table>
+    <tr>
+      <th><a href="about.php">one</a></th>
+      <th><a href="">two</a></th>
+      <th><a href="">three</a></th>
+      <th><a href="">four</a></th>
+    </tr>
+  </table>
 </body>
 </html>
